80b1216a596ad7b9c178ef00a3606cb5b3026ca8
Jake B <jakeburke1775@gmail.com>
2025-07-12 06:50:57 -0400

    add header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,18 @@
+import logo from "../../assets/react.svg"; // or the correct image file in assets
+import avatar from "../../assets/avatar.png"; // if avatar.png exists
+
 function Header() {
-  return <header>HEADER</header>;
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} />
+      <p className="header__date-n-locate">DATE, LOCATION</p>
+      <button className="header__add-clothes">+ Add clothes</button>
+      <div className="header__user-container">
+        <p className="header__username">Name</p>
+        <img src={avatar} className="header__user-avatar" />
+      </div>
+    </header>
+  );
 }
 
 export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./vendor/normalize.css";
 import "./index.css";
 import App from "../src/components/App/App";
 
